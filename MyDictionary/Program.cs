514ebcd7fa741b61c83901a54cf128165f499d4c
muaz52 <61652847+muaz52@users.noreply.github.com>
2021-03-05 06:46:01 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> students = new MyDictionary<int, string>();
+
+            students.Add(163, "Arif");
+            students.Add(120, "Yusuf");
+            students.Add(150, "Harun");
+            students.Add(556, "Emre");
+
+
+            students.ToList();
+        }
+    }
+}
